d380a0f0a288f7b384ad3c12d66ae268dd20e737
Alexandra Norcross <norcrossalex@gmail.com>
2015-11-02 16:19:43 -0800

    First commit

---
+++ CrackingTheCodeQuestions/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrackingTheCodeQuestions
+//
+//  Created by Alexandra Norcross on 11/2/15.
+//  Copyright © 2015 Alexandra Norcross. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrackingTheCodeQuestions/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CrackingTheCodeQuestions
+//
+//  Created by Alexandra Norcross on 11/2/15.
+//  Copyright © 2015 Alexandra Norcross. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
